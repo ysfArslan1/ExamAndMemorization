04f13aa41483560aab118e069739c46ccdd1b1da
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-01-25 22:52:45 +0300

    Create UserCqrs.cs

---
+++ QAM.Wapi/QAM.Business/Cqrs/UserCqrs.cs
@@ -0,0 +1,14 @@
+using MediatR;
+using QAM.Base.Response;
+using QAM.Scheme;
+
+namespace QAM.Business.Cqrs;
+
+
+public record CreateUserCommand(int CurrentUserId, CreateUserRequest Model) : IRequest<ApiResponse<UserResponse>>;
+public record UpdateUserCommand(int Id, int CurrentUserId, UpdateUserRequest Model) : IRequest<ApiResponse>;
+public record DeleteUserCommand(int Id) : IRequest<ApiResponse>;
+
+public record GetAllUserQuery() : IRequest<ApiResponse<List<UserResponse>>>;
+public record GetUserByIdQuery(int Id) : IRequest<ApiResponse<UserResponse>>;
+
