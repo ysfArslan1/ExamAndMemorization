4c177a86088f1915557c2a72eea6b990bd34c5ce
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-01-28 22:19:51 +0300

    controllers added

---
+++ QAM.Wapi/QAM.Wapi/Controllers/FavoriteController.cs
@@ -0,0 +1,82 @@
+using QAM.Base.Response;
+using QAM.Business.Cqrs;
+using QAM.Business.Validator;
+using QAM.Data;
+using QAM.Data.Entity;
+using QAM.Scheme;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace QAM.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FavoriteController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public FavoriteController(IMediator _mediator)
+    {
+        mediator = _mediator;
+    }
+
+    [HttpGet]
+    public async Task<ApiResponse<List<FavoriteResponse>>> Get()
+    {
+        var operation = new GetAllFavoriteQuery();
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database bulunan Favorite verilerinin çekilmasi için kullanýlýr.
+    [HttpGet("{id}")]
+    public async Task<ApiResponse<FavoriteResponse>> Get(int id)
+    {
+        var operation = new GetFavoriteByIdQuery(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database de Favorite verisi oluþturmak için kullanýlýr.
+    [HttpPost]
+    public async Task<ApiResponse<FavoriteResponse>> Post([FromBody] CreateFavoriteRequest Favorite)
+    {
+        // Validation iþlemi uygulanýr
+        CreateFavoriteRequestValidator validator = new CreateFavoriteRequestValidator();
+        validator.ValidateAndThrow(Favorite);
+
+        string _id = (User.Identity as ClaimsIdentity).FindFirst("Id")?.Value;
+        int CurrentFavoriteId = int.Parse(_id);
+
+        var operation = new CreateFavoriteCommand(CurrentFavoriteId, Favorite);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database den id degeri verilen Favorite verisi alýnmak için kullanýlýr.
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] UpdateFavoriteRequest Favorite)
+    {
+        // Validation iþlemi uygulanýr
+        UpdateFavoriteRequestValidator validator = new UpdateFavoriteRequestValidator();
+        validator.ValidateAndThrow(Favorite);
+
+        string _id = (User.Identity as ClaimsIdentity).FindFirst("Id")?.Value;
+        int CurrentFavoriteId = int.Parse(_id);
+
+        var operation = new UpdateFavoriteCommand(id,CurrentFavoriteId, Favorite);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database den id degeri verilen Favorite verisi softdelete yapýlýr
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteFavoriteCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+}
---
+++ QAM.Wapi/QAM.Wapi/Controllers/TagController.cs
@@ -0,0 +1,82 @@
+using QAM.Base.Response;
+using QAM.Business.Cqrs;
+using QAM.Business.Validator;
+using QAM.Data;
+using QAM.Data.Entity;
+using QAM.Scheme;
+using FluentValidation;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace QAM.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TagController : ControllerBase
+{
+    private readonly IMediator mediator;
+    public TagController(IMediator _mediator)
+    {
+        mediator = _mediator;
+    }
+
+    [HttpGet]
+    public async Task<ApiResponse<List<TagResponse>>> Get()
+    {
+        var operation = new GetAllTagQuery();
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database bulunan Tag verilerinin çekilmasi için kullanýlýr.
+    [HttpGet("{id}")]
+    public async Task<ApiResponse<TagResponse>> Get(int id)
+    {
+        var operation = new GetTagByIdQuery(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database de Tag verisi oluþturmak için kullanýlýr.
+    [HttpPost]
+    public async Task<ApiResponse<TagResponse>> Post([FromBody] CreateTagRequest Tag)
+    {
+        // Validation iþlemi uygulanýr
+        CreateTagRequestValidator validator = new CreateTagRequestValidator();
+        validator.ValidateAndThrow(Tag);
+
+        string _id = (User.Identity as ClaimsIdentity).FindFirst("Id")?.Value;
+        int CurrentTagId = int.Parse(_id);
+
+        var operation = new CreateTagCommand(CurrentTagId, Tag);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database den id degeri verilen Tag verisi alýnmak için kullanýlýr.
+    [HttpPut("{id}")]
+    public async Task<ApiResponse> Put(int id, [FromBody] UpdateTagRequest Tag)
+    {
+        // Validation iþlemi uygulanýr
+        UpdateTagRequestValidator validator = new UpdateTagRequestValidator();
+        validator.ValidateAndThrow(Tag);
+
+        string _id = (User.Identity as ClaimsIdentity).FindFirst("Id")?.Value;
+        int CurrentTagId = int.Parse(_id);
+
+        var operation = new UpdateTagCommand(id,CurrentTagId, Tag);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+    // Database den id degeri verilen Tag verisi softdelete yapýlýr
+    [HttpDelete("{id}")]
+    public async Task<ApiResponse> Delete(int id)
+    {
+        var operation = new DeleteTagCommand(id);
+        var result = await mediator.Send(operation);
+        return result;
+    }
+
+}
