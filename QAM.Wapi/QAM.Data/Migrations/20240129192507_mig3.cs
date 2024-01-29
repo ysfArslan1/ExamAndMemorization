bb1f0c4f59eafa39c35f03ce5bf4175bce7353d6
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-01-29 22:56:29 +0300

    migration added

---
+++ QAM.Wapi/QAM.Data/Migrations/20240129192507_mig3.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QAM.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "isPublic",
+                table: "Subjects",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldDefaultValue: "False");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "isPublic",
+                table: "Subjects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "False",
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
